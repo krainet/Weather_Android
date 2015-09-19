6d9aa621659d939217d5b17222d70c1f0d351b5c
Ramón Albertí <krainet@gmail.com>
2015-09-19 14:32:49 +0200

    Image changing with button

--- app/src/main/java/com/develjitsu/guedr/MainActivity.java
+++ app/src/main/java/com/develjitsu/guedr/MainActivity.java
@@ -2,6 +2,10 @@ package com.develjitsu.guedr;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +13,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ImageView imageView = (ImageView) findViewById(R.id.weather_image);
+        Button changeButton = (Button) findViewById(R.id.change_system_button);
+        changeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.v("RAMON1", "Change Image using TAG");
+                Log.v("RAMON2", String.valueOf(imageView.getTag()));
+                if(imageView.getTag()==(Integer) 1){
+                    Log.v("RAMON3", String.valueOf(imageView.getTag()));
+                    imageView.setImageResource(R.drawable.offline_weather2);
+                    imageView.setTag(2);
+                }else{
+                    Log.v("RAMON4", String.valueOf(imageView.getTag()));
+                    imageView.setImageResource(R.drawable.offline_weather);
+                    imageView.setTag(1);
+                }
+                Log.v("RAMON5", String.valueOf(imageView.getTag()));
+
+
+            }
+        });
+
+
     }
 
 
