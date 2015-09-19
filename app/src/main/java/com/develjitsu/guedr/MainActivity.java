2164ff0d637fce88d4cb34adc849945da6d4735f
Ramón Albertí <krainet@gmail.com>
2015-09-19 13:41:55 +0200

    First offline version

--- app/src/main/java/com/develjitsu/guedr/MainActivity.java
+++ app/src/main/java/com/develjitsu/guedr/MainActivity.java
@@ -1,9 +1,7 @@
 package com.develjitsu.guedr;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,7 +11,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    @Override
+
+
+    //quitar menu
+    /*@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
@@ -33,5 +34,5 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
+    }*/
 }
