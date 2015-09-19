382d1c73b6ad3bbf9dc08599b848107dca00188d
Ramón Albertí <krainet@gmail.com>
2015-09-19 17:51:28 +0200

    First model OK

--- app/src/main/java/com/develjitsu/guedr/activity/ForecastActivity.java
+++ app/src/main/java/com/develjitsu/guedr/activity/ForecastActivity.java
@@ -2,42 +2,43 @@ package com.develjitsu.guedr.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.ImageView;
-import android.widget.ToggleButton;
+import android.widget.TextView;
 
 import com.develjitsu.guedr.R;
+import com.develjitsu.guedr.model.Forecast;
 
 public class ForecastActivity extends AppCompatActivity {
 
-    private ImageView mImageView;
+    private Forecast mForecast;
+
+    private ImageView mIcon;
+    private TextView mMaxTemp;
+    private TextView mMinTemp;
+    private TextView mHumidity;
+    private TextView mDescription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forecast);
 
-        mImageView= (ImageView) findViewById(R.id.weather_image);
-        final ToggleButton button = (ToggleButton) findViewById(R.id.system_button3);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mImageView.setImageResource(button.isChecked()?R.drawable.offline_weather:R.drawable.offline_weather2);
-            }
-        });
+        mIcon= (ImageView) findViewById(R.id.forecast_image);
+        mMaxTemp= (TextView) findViewById(R.id.max_temp);
+        mMinTemp= (TextView) findViewById(R.id.min_temp);
+        mHumidity= (TextView) findViewById(R.id.humidity);
+        mDescription= (TextView) findViewById(R.id.forecast_description);
+
+        setForecast(new Forecast(37,23,59,"Algunas nuves","icon01"));
     }
 
+    public void setForecast(Forecast forecast){
+        mForecast = forecast;
 
+        mMaxTemp.setText(String.format(getString(R.string.max_temp_parameter),forecast.getMaxTemp()));
+        mMinTemp.setText(String.format(getString(R.string.min_temp_parameter),forecast.getMaxTemp()));
+        mHumidity.setText(String.format(getString(R.string.humidity_parameter), forecast.getMaxTemp()));
+        mDescription.setText(forecast.getDescription());
 
-/*    @Override
-    public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.system_button1:
-                mImageView.setImageResource(R.drawable.offline_weather2);
-                break;
-            case R.id.system_button2:
-                mImageView.setImageResource(R.drawable.offline_weather);
-                break;
-        }
-    }*/
+    }
 }
