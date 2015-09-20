c00cd10900695de7145a722dff9313c405d1d756
Ramón Albertí <krainet@gmail.com>
2015-09-20 12:25:48 +0200

    Insert activity into fragment

--- app/src/main/java/com/develjitsu/guedr/activity/ForecastActivity.java
+++ app/src/main/java/com/develjitsu/guedr/activity/ForecastActivity.java
@@ -1,113 +1,16 @@
 package com.develjitsu.guedr.activity;
 
-import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.develjitsu.guedr.R;
-import com.develjitsu.guedr.model.Forecast;
 
 public class ForecastActivity extends AppCompatActivity {
-
-    private Forecast mForecast;
-
-    private ImageView mIcon;
-    private TextView mMaxTemp;
-    private TextView mMinTemp;
-    private TextView mHumidity;
-    private TextView mDescription;
-
-    private int mCurrentMetrics;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forecast);
-
-        mIcon= (ImageView) findViewById(R.id.forecast_image);
-        mMaxTemp= (TextView) findViewById(R.id.max_temp);
-        mMinTemp= (TextView) findViewById(R.id.min_temp);
-        mHumidity= (TextView) findViewById(R.id.humidity);
-        mDescription= (TextView) findViewById(R.id.forecast_description);
-
-        setForecast(new Forecast(37, 23, 59, "Algunas nuves", "icon01"));
-        mCurrentMetrics=getSelectedMetrics();
     }
 
-    public void setForecast(Forecast forecast){
-        mForecast = forecast;
-
-        float maxTmp = mCurrentMetrics == SettingsActivity.PREF_CELSIUS?forecast.getMaxTemp():toFarenheit(forecast.getMaxTemp());
-        float minTmp = mCurrentMetrics == SettingsActivity.PREF_CELSIUS?forecast.getMinTemp():toFarenheit(forecast.getMinTemp());
-        String tmpSuffix = mCurrentMetrics == SettingsActivity.PREF_CELSIUS?"ºC":"ºF";
-
-        mMaxTemp.setText(String.format(getString(R.string.max_temp_parameter),maxTmp,tmpSuffix));
-        mMinTemp.setText(String.format(getString(R.string.min_temp_parameter),minTmp,tmpSuffix));
-        mHumidity.setText(String.format(getString(R.string.humidity_parameter), forecast.getHumidity()));
-        mDescription.setText(forecast.getDescription());
-
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.forecast,menu);
-        //return super.onCreateOptionsMenu(menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if(item.getItemId()==R.id.menu_settings){
-            Intent settingsIntent = new Intent(this,SettingsActivity.class);
-            startActivity(settingsIntent);
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        if(mCurrentMetrics!=getSelectedMetrics()){
-            final int prevMetrics = mCurrentMetrics;
-
-            mCurrentMetrics=getSelectedMetrics();
-            setForecast(mForecast);
-
-            //Snack bar notification (with undo)
-            Snackbar.make(findViewById(android.R.id.content), R.string.updated_preferences,Snackbar.LENGTH_LONG)
-                    .setAction(R.string.undo, new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            //Get preferences instade of create instance of SharedPreferences (using static mode)
-                            getPreferences(MODE_PRIVATE).edit()
-                                    .putString(getString(R.string.metric_selection),String.valueOf(prevMetrics))
-                                    .apply(); //Puede usarse commit(), apply() no comprueba error al guardar
-                            mCurrentMetrics=prevMetrics;
-                            setForecast(mForecast);
-                        }
-                    }).show();
-        }
-    }
-
-    private int getSelectedMetrics() {
-        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this);
-        String stringMetrics = pref.getString(getString(R.string.metric_selection), String.valueOf(SettingsActivity.PREF_CELSIUS));
-        int metrics = Integer.valueOf(stringMetrics);
-        return metrics;
-    }
-
-    public static float toFarenheit(float celsius){
-        return (celsius*1.8f)+32;
-    }
 }
---
+++ app/src/main/java/com/develjitsu/guedr/fragment/ForecastFragment.java
@@ -0,0 +1,133 @@
+package com.develjitsu.guedr.fragment;
+
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.develjitsu.guedr.R;
+import com.develjitsu.guedr.activity.SettingsActivity;
+import com.develjitsu.guedr.model.Forecast;
+
+/**
+ * Created by hadock on 20/09/15.
+ */
+public class ForecastFragment extends Fragment {
+
+    private Forecast mForecast;
+
+    private ImageView mIcon;
+    private TextView mMaxTemp;
+    private TextView mMinTemp;
+    private TextView mHumidity;
+    private TextView mDescription;
+
+    private int mCurrentMetrics;
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+
+        View root = inflater.inflate(R.layout.fragment_forecast,container,false);
+
+        mIcon= (ImageView) root.findViewById(R.id.forecast_image);
+        mMaxTemp= (TextView) root.findViewById(R.id.max_temp);
+        mMinTemp= (TextView) root.findViewById(R.id.min_temp);
+        mHumidity= (TextView) root.findViewById(R.id.humidity);
+        mDescription= (TextView) root.findViewById(R.id.forecast_description);
+
+        mCurrentMetrics=getSelectedMetrics();
+
+        setForecast(new Forecast(37, 23, 59, "Algunas nuves", "icon01"));
+
+        return root;
+    }
+
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu,MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu,inflater);
+        inflater.inflate(R.menu.forecast, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId()==R.id.menu_settings){
+            Intent settingsIntent = new Intent(getActivity(),SettingsActivity.class);
+            startActivity(settingsIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private int getSelectedMetrics() {
+        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        String stringMetrics = pref.getString(getString(R.string.metric_selection), String.valueOf(SettingsActivity.PREF_CELSIUS));
+        int metrics = Integer.valueOf(stringMetrics);
+        return metrics;
+    }
+
+    public static float toFarenheit(float celsius){
+        return (celsius*1.8f)+32;
+    }
+
+    public void setForecast(Forecast forecast){
+        mForecast = forecast;
+
+        float maxTmp = mCurrentMetrics == SettingsActivity.PREF_CELSIUS?forecast.getMaxTemp():toFarenheit(forecast.getMaxTemp());
+        float minTmp = mCurrentMetrics == SettingsActivity.PREF_CELSIUS?forecast.getMinTemp():toFarenheit(forecast.getMinTemp());
+        String tmpSuffix = mCurrentMetrics == SettingsActivity.PREF_CELSIUS?"ºC":"ºF";
+
+        mMaxTemp.setText(String.format(getString(R.string.max_temp_parameter),maxTmp,tmpSuffix));
+        mMinTemp.setText(String.format(getString(R.string.min_temp_parameter),minTmp,tmpSuffix));
+        mHumidity.setText(String.format(getString(R.string.humidity_parameter), forecast.getHumidity()));
+        mDescription.setText(forecast.getDescription());
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        if(mCurrentMetrics!=getSelectedMetrics()){
+            final int prevMetrics = mCurrentMetrics;
+
+            mCurrentMetrics=getSelectedMetrics();
+            setForecast(mForecast);
+
+            //Snack bar notification (with undo)
+            Snackbar.make(getView().findViewById(android.R.id.content), R.string.updated_preferences, Snackbar.LENGTH_LONG)
+                    .setAction(R.string.undo, new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            //Get preferences instade of create instance of SharedPreferences (using static mode)
+                            getActivity().getPreferences(1).edit()
+                                    .putString(getString(R.string.metric_selection),String.valueOf(prevMetrics))
+                                    .apply(); //Puede usarse commit(), apply() no comprueba error al guardar
+                            mCurrentMetrics=prevMetrics;
+                            setForecast(mForecast);
+                        }
+                    }).show();
+        }
+    }
+}
